b239f661b6a79f71a3d33085a9ae710f6f169837
Evan D <evan.delasota@gmail.com>
2021-06-09 01:45:23 +0000

    Optimized solution using Sieve of Eratosthenes, runtime of O(n*log(log(n))

--- Main.java
+++ Main.java
@@ -4,22 +4,27 @@ class Main {
   }
 
   public static boolean isPrime(int n) {
-    if (n == 1) {
-      return false;
-    }
-    if (n == 2) {
-      return true;
-    }
-    if (n % 2 == 0) {
-      return false;
-    }
-
-    for (int i = 2; i * i <= n; ++i) {
-      if (n % i == 0) {
-        return false;
+    int[] prime = new int[n + 1];
+    for(int i = 3; i <= n; ++i) {
+      if (prime[i] % 2 == 0) {
+        prime[i] = 0;
+      } else {
+        prime[i] = 1;
       }
     }
 
-    return true;
+    for (int i = 3; i <= n; ++i) {
+      if (prime[i]) {
+        for (int j = 2 * i; j <= n; j += i) {
+          prime[j] = 0;
+        }
+      }
+    }
+
+    prime[0] = 0;
+    prime[1] = 0;
+    prime[2] = 0;
+
+    return (prime[n] == 1) ? true : false;
   }
 }
